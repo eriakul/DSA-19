67cf1b921eb0b95d418a77b19c49883298a54365
eriakul <lu.erika58@gmail.com>
2019-02-10 21:19:45 -0500

    hw

--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -39,49 +39,6 @@ public class Problems {
         }
 
     }
-//    public static List<Integer> removeKDigitsHelper(List list, int index, int k) {
-//        if (k == 0){
-//            return list;
-//        }
-//        if (index + k <= list.size() - 1){
-//            if ((int) list.get(index) <= (int) list.get(index+k)){
-//                return removeKDigitsHelper(list, index + 1, k);
-//            }
-//            else if ((int) list.get(index) > (int) list.get(index+k)){
-//                list.remove(index);
-//                return removeKDigitsHelper(list, index, k-1);
-//            }
-//            else{
-//                System.out.println("Weird case encountered.");
-//                return list;
-//            }
-//        }
-//        else{
-//            return list.subList(0, index);
-//        }
-//
-//    }
-
-//    public static List<Integer> removeKDigitsHelper(List list, int k) {
-//        if (k == 0){
-//            return list;
-//        }
-//        if ((int) list.get(0) >= (int) list.get(k)){
-//            list.remove(0);
-//            return removeKDigitsHelper(list, k - 1);
-//        }
-//        else if ((int) list.get(0) < (int) list.get(k)){
-//            list.remove(k);
-//            return removeKDigitsHelper(list, k-1);
-//        }
-//        else{
-//            System.out.println("Weird case encountered.");
-//            return list;
-//        }
-//
-//    }
-
-
 
 
     public static List<Integer> removeKDigits(int[] A, int k) {
@@ -94,14 +51,74 @@ public class Problems {
         return list;
     }
 
+    public static boolean isPalindromeHelper(Node node, boolean forward, String string, Node last_node) {
+        if (node == null){
+            return true;
+        }
+
+        if (node.next == null && !forward){
+            return true;
+        }
+        if (node.next == null && forward){
+            string = string + node.val;
+            if (Integer.parseInt(string.substring(0,1)) != node.val){
+                return false;
+            }
+            else {
+                return isPalindromeHelper(last_node, false, string.substring(1), null);
+            }
+        }
+        if (forward){
+            Node next_node = node.next;
+            node.next = last_node;
+            if (last_node == null){
+            node.next = null;
+            }
+            return isPalindromeHelper(next_node, forward, string + node.val, node);
+        }
+        else{
+            if (string == ""){
+                return true;
+            }
+            if (Integer.parseInt(string.substring(0,1)) != node.val){
+                return false;
+            }
+            else {
+                return isPalindromeHelper(node.next, forward, string.substring(1), null);
+            }
+        }
+    }
+
+
+
     public static boolean isPalindrome(Node n) {
-        // TODO: your code here
-        return false;
+        return isPalindromeHelper(n, true, "", null);
+    }
+
+    public static String infixToPostfixHelper(int pf, int pe, String string) {
+        pf += 2;
+        pe -= 2;
+        if (string.charAt(pf) == '(' || string.charAt(pf) == ')'){
+            char num = string.charAt(pe );
+            char op = string.charAt(pe-2);
+            return infixToPostfixHelper(pf, pe-4, string) + String.format(" %c %c", num, op);
+        }
+        else if (string.charAt(pe) == '(' || string.charAt(pe) == ')'){
+            char num = string.charAt(pf);
+            char op = string.charAt(pf+2);
+            return String.format("%c %s %c", num, infixToPostfixHelper(pf+4, pe, string), op);
+        }
+        else{
+            char num1 = string.charAt(pf);
+            char op = string.charAt(pf+2);
+            char num2 = string.charAt(pf+4);
+            return String.format("%c %c %c", num1, num2, op);
+        }
     }
 
     public static String infixToPostfix(String s) {
-        // TODO
-        return null;
+
+        return infixToPostfixHelper(0, s.length()-1, s);
     }
 
 }
